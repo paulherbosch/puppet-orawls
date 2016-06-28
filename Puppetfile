e2b8e4748efd68b7d206d642fd4c02261889d56f
fdammeke <fabian.dammekens@cegeka.be>
2016-06-28 10:58:56 +0200

    [SPEC] Fix tests

---
+++ Puppetfile
@@ -0,0 +1,10 @@
+mod 'easy_type',
+  :git => 'https://github.com/cegeka/puppet-easy_type'
+mod 'filemapper',
+  :git => 'https://github.com/cegeka/puppet-filemapper'
+mod 'yamlfile',
+  :git => 'https://github.com/cegeka/puppet-yamlfile'
+mod 'sleep',
+  :git => 'https://github.com/cegeka/puppet-sleep'
+mod 'stdlib',
+  :git => 'https://github.com/cegeka/puppet-stdlib'
---
+++ spec/acceptance/domain_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper_acceptance'
+
+describe 'orawls::domain' do
+
+  describe 'running puppet code' do
+    it 'should work with no errors' do
+      pp = <<-EOS
+        include orawls::urandomfix
+        class { 'orawls::weblogic': }
+        class { 'orawls::domain':
+          version           => 1036,
+          weblogic_home_dir => '/opt/weblogic/11g/wlserver_10.3',
+          middleware_home_dir => '/u01/weblogic/11g',
+          jdk_home_dir        => '/usr/java/jdk1.6.0_45',
+          domain_template     => 'standard',
+          domain_name         => 'TESTdomain',
+          development_mode    => false,
+          adminserver_name    => 'AdminServer',
+          adminserver_address => 'localhost',
+          adminserver_port    => 7011,
+          nodemanager_secure_listener => true,
+          nodemanager_port    => 5556,
+          weblogic_user       => 'weblogic',
+          weblogic_password   => 'weblogic',
+          os_user             => 'root',
+          os_group            => 'root',
+          log_dir             => '/var/log',
+          download_dir        => '/tmp',
+          log_output          => true,
+          require             => Class['orawls::weblogic']
+        }
+      EOS
+
+      # Run it twice and test for idempotency
+      apply_manifest(pp, :catch_failures => true)
+      apply_manifest(pp, :catch_changes => true)
+    end
+
+    describe file '/opt/weblogic/11g/wlserver_10.3' do
+      it { is_expected.to be_directory }
+    end
+
+  end
+end
+
